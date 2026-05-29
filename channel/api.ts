b3cf83beb7baaa89eddd3c56e32e978b7017398b
cbackup1986-dev <cbackup1986@gmail.com>
2026-05-29 10:08:57 +0800

    chore: ignore channel and shell folders

--- channel/api.ts
+++
@@ -1,6 +0,0 @@
-export * from "./src/accounts.js";
-export * from "./src/channel.js";
-export * from "./src/runtime.js";
-export * from "./src/runtime-api.js";
-export { getTukenRuntime, setTukenRuntime } from "./src/runtime.js";
-
--- channel/setup-entry.ts
+++
@@ -1,5 +0,0 @@
-import { defineSetupPluginEntry } from "openclaw/plugin-sdk/channel-core";
-import { tukenPlugin } from "./src/channel.js";
-
-export default defineSetupPluginEntry(tukenPlugin);
-
