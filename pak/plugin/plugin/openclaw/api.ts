77a30c0990e8b4bf993fb49e6a5b2a75bc06148a
cbackup1986-dev <cbackup1986@gmail.com>
2026-05-29 10:58:26 +0800

    chore: remove unused pak directory

--- pak/plugin/plugin/openclaw/api.ts
+++
@@ -1,5 +0,0 @@
-export * from "./src/accounts.js";
-export * from "./src/channel.js";
-export * from "./src/runtime.js";
-export * from "./src/runtime-api.js";
-export { getClawbotHubRuntime, setClawbotHubRuntime } from "./src/runtime.js";
--- pak/plugin/plugin/openclaw/setup-entry.ts
+++
@@ -1,4 +0,0 @@
-import { defineSetupPluginEntry } from "openclaw/plugin-sdk/channel-core";
-import { clawbotHubPlugin } from "./src/channel.js";
-
-export default defineSetupPluginEntry(clawbotHubPlugin);
